943c4ccdadd169b73b87fc48e74cd3b170f90adf
ilyabezdelev <ilya.bezdelev@gmail.com>
2025-02-08 14:44:04 +0300

    Checking in utf-8 contents

---
+++ .tmp/src_utf8/page/menu.js
@@ -0,0 +1,106 @@
+function top(title) {
+
+document.write('<body background="img/bkg.gif" bgcolor="#000000" text="#000000" link="#000080"');
+document.write('vlink="#000000" alink="#FF0000">');
+document.write('<div align="center"><center>');
+document.write('');
+document.write('<table border="0" cellpadding="0" cellspacing="0" width="620" bgcolor="#E3DEC8">');
+document.write('  <tr>');
+document.write('    <td width="42" valign="top" bgcolor="#000000" height="1" colspan="5"><img src="pix.gif"');
+document.write('    width="1" height="1"></td>');
+document.write('  </tr>');
+document.write('  <tr>');
+document.write('    <td width="1" valign="top" bgcolor="#000000" rowspan="11"><img src="pix.gif" width="1"');
+document.write('    height="1"></td>');
+document.write('    <td width="20" valign="top" bgcolor="#FFFFFF"><img src="pix.gif" width="1" height="1"></td>');
+document.write('    <td valign="top" bgcolor="#FFFFFF"><img src="img/irk.gif" width="17" height="64"><img');
+document.write('    src="img/logotype.gif" width="251" height="64"><img src="img/spacer.gif"');
+document.write('    ><img src="title/' +title+ '"></td>');
+document.write('    <td width="20" valign="top" bgcolor="#FFFFFF"><img src="pix.gif" width="1" height="1"></td>');
+document.write('    <td width="1" valign="top" bgcolor="#000000" rowspan="11"><img src="pix.gif" width="1"');
+document.write('    height="1"></td>');
+document.write('  </tr>');
+document.write('  <tr>');
+document.write('    <td width="40" valign="top" bgcolor="#000000" height="1" colspan="3"><img src="pix.gif"');
+document.write('    width="1" height="1"></td>');
+document.write('  </tr>');
+document.write('  <tr>');
+document.write('    <td width="20" valign="top"><img src="pix.gif" width="1" height="1"></td>');
+document.write('    <td valign="top"><p align="center"><span class="menufont"><a href="index.html"');
+document.write('    class="menufont">Новости</a> <a href="members.html" class="menufont">Члены</a>');
+document.write('    <a href="history.html" class="menufont">История</a> <a href="wars.html"');
+document.write('    class="menufont">Войны</a> <a href="champs.html" class="menufont">Чемпы</a> <a');
+document.write('    href="demos.html" class="menufont">Демки</a> <a href="files.html" class="menufont">Файлы</a>');
+document.write('    <a href="photos.html" class="menufont">Фотки</a> <a href="links.html"');
+document.write('    class="menufont">Ссылки</a> <a href="articles.html" class="menufont">Статьи</a>');
+document.write('    <a href="board.html" class="menufont">Борда</a> <a');
+document.write('    href="http://www.guestbook.ru/book.cgi?action=show&amp;user=38clan" class="menufont"');
+document.write('    >Книга</a></span></td>');
+document.write('    <td width="20" valign="top"><img src="pix.gif" width="1" height="1"></td>');
+document.write('  </tr>');
+document.write('  <tr>');
+document.write('    <td width="40" valign="top" bgcolor="#000000" height="1" colspan="3"><img src="pix.gif"');
+document.write('    width="1" height="1"></td>');
+document.write('  </tr>');
+document.write('  <tr>');
+document.write('    <td width="20" valign="top" rowspan="3"><img src="pix.gif" width="1" height="1"><p><img');
+document.write('    src="pix.gif" width="1" height="1"></p>');
+document.write('    <p><img src="pix.gif" width="1" height="1"></td>');
+document.write('    <td valign="top" bgcolor="#E3DEC8">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </td>');
+document.write('    <td width="20" valign="top" rowspan="3"><img src="pix.gif" width="1" height="1"><p><img');
+document.write('    src="pix.gif" width="1" height="1"></p>');
+document.write('    <p><img src="pix.gif" width="1" height="1"></td>');
+document.write('  </tr>');
+document.write('  <tr>');
+document.write('    <td valign="top" bgcolor="#E3DEC8">');
+
+}
+
+
+
+function bottom() {
+
+document.write('</td>');
+document.write('  </tr>');
+document.write('  <tr>');
+document.write('    <td valign="top">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </td>');
+document.write('  </tr>');
+document.write('  <tr>');
+document.write('    <td width="40" valign="top" bgcolor="#000000" height="1" colspan="3"><img src="pix.gif"');
+document.write('    width="1" height="1"></td>');
+document.write('  </tr>');
+document.write('  <tr>');
+document.write('    <td width="20" valign="top"><img src="pix.gif" width="1" height="1"></td>');
+document.write('    <td valign="top"><p align="center"><span class="menufont"><a href="index.html"');
+document.write('    class="menufont">Новости</a> <a href="members.html" class="menufont">Члены</a>');
+document.write('    <a href="history.html" class="menufont">История</a> <a href="wars.html"');
+document.write('    class="menufont">Войны</a> <a href="champs.html" class="menufont">Чемпы</a> <a');
+document.write('    href="demos.html" class="menufont">Демки</a> <a href="files.html" class="menufont">Файлы</a>');
+document.write('    <a href="photos.html" class="menufont">Фотки</a> <a href="links.html"');
+document.write('    class="menufont">Ссылки</a> <a href="articles.html" class="menufont">Статьи</a>');
+document.write('    <a href="board.html" class="menufont">Борда</a> <a');
+document.write('    href="http://www.guestbook.ru/book.cgi?action=show&amp;user=38clan" class="menufont"');
+document.write('    >Книга</a></span></td>');
+document.write('    <td width="20" valign="top"><img src="pix.gif" width="1" height="1"></td>');
+document.write('  </tr>');
+document.write('  <tr>');
+document.write('    <td width="40" valign="top" bgcolor="#000000" height="1" colspan="3"><img src="pix.gif"');
+document.write('    width="1" height="1"></td>');
+document.write('  </tr>');
+document.write('  <tr>');
+document.write('    <td width="20" valign="top"><img src="pix.gif" width="1" height="1"></td>');
+document.write('    <td valign="top"><p align="right"><span class="menufont">© <a');
+document.write('    href="mailto:voodoo@38net.ru" class="menufont">38 [VooDoo]</a>, 1999-2000. <a');
+document.write('    href="http://38clan.38net.ru" class="menufont">http://38clan.38net.ru</a>. Hosted by <a');
+document.write('    href="http://www.38net.ru" class="menufont" target="_blank">Internet cafe 38Net</a>.</span></td>');
+document.write('    <td width="20" valign="top"><img src="pix.gif" width="1" height="1"></td>');
+document.write('  </tr>');
+document.write('  <tr>');
+document.write('    <td width="42" valign="top" bgcolor="#000000" height="1" colspan="5"><img src="pix.gif"');
+document.write('    width="1" height="1"></td>');
+document.write('  </tr>');
+document.write('</table>');
+document.write('</center></div>');
+document.write('</body>');
+
+}
